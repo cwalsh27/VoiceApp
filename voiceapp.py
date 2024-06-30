50450dc2dfa7a6073d6649dcec339d00e170122c
Coleman <cowalsh@udel.edu>
2024-06-29 22:55:14 -0400

    fully functional script implementation, with storage issue described in issues

--- voiceapp.py
+++ voiceapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect, send_from_directory
+from flask import Flask, render_template, url_for, flash, redirect, send_from_directory, send_file
 from forms import RegistrationForm, LoginForm, UploadDocForm
 from werkzeug.utils import secure_filename
 from MicrophenoTextProcessor import check_valid_run, process_doc
@@ -27,7 +27,7 @@ def appone():
         flash(f'Succesfully uploaded file named {file.name}!', 'success')
 
         newFile = process_doc(file)
-        #newFile.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename('micropheno_output_text.docx'))) 
+        newFile.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename('micropheno_output_text.docx'))) 
         return redirect(url_for('download_file', filename='micropheno_output_text.docx'))
     elif form.is_submitted(): 
         flash('Upload Unsuccessful. Please confirm that you are attempting to upload a text file of type .docx', 'danger')
@@ -37,6 +37,11 @@ def appone():
 @app.route('/uploads/<filename>')
 def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    # @app.after_request
+    # def delete(response):
+    #     os.remove()
+    
+    
 
 if __name__ == '__main__': 
     app.run(debug=True)
